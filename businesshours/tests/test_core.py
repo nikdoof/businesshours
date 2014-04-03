1b298855059a21ff01d32ad824f861404487c34f
Andrew Williams <andy@tensixtyone.com>
2014-04-03 14:49:01 +0100

    Added further tests to check the weekend/weekday cutover.

--- businesshours/tests/test_core.py
+++ businesshours/tests/test_core.py
@@ -11,7 +11,7 @@ class TestCalcBusinessHours(unittest.TestCase):
                 self.assertEqual(calc_business_hours(dt1, dt2,), res)
 
         def test_invalid_arguments(self):
-            self.assertRaises(ValueError, calc_business_hours, datetime(2014,1,2), datetime(2014,1,1))
+            self.assertRaises(ValueError, calc_business_hours, datetime(2014, 1, 2), datetime(2014, 1, 1))
 
         def test_simple_day(self):
             self.run_tests([
@@ -30,7 +30,14 @@ class TestCalcBusinessHours(unittest.TestCase):
 
         def test_weekend_single_day(self):
             self.run_tests([
-               (0, datetime(2013, 8, 24, 9, 0, 0), datetime(2013, 8, 24, 9, 1, 0)),
-               (0, datetime(2013, 8, 25, 9, 0, 0), datetime(2013, 8, 25, 9, 10, 0)),  # Multiday during weekend
-               (0, datetime(2013, 8, 3, 7, 0, 0), datetime(2013, 8, 4, 9, 0, 0)),
+                (0, datetime(2013, 8, 24, 9, 0, 0), datetime(2013, 8, 24, 9, 1, 0)),
+                (0, datetime(2013, 8, 25, 9, 0, 0), datetime(2013, 8, 25, 9, 10, 0)),  # Multiday during weekend
+                (0, datetime(2013, 8, 3, 7, 0, 0), datetime(2013, 8, 4, 9, 0, 0)),
+            ])
+
+        def test_weekend_early_monday(self):
+            self.run_tests([
+                (0, datetime(2014, 4, 6, 23, 59, 59), datetime(2014, 4, 7, 0, 0, 59)),
+                (59, datetime(2014, 4, 6, 23, 59, 59), datetime(2014, 4, 7, 8, 0, 59)),
+                (0, datetime(2014, 4, 4, 23, 59, 59), datetime(2014, 4, 5, 8, 0, 59))
             ])
